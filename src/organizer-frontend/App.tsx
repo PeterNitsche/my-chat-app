65666fcc43da724650f62e593587b166b950c9d3
Peter Nitsche <peter.nitsche@gmail.com>
2024-02-25 14:15:58 +0100

    send random message to websocket server

--- src/organizer-frontend/App.tsx
+++ src/organizer-frontend/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
 
+import useWebSocket from "react-use-websocket";
+
+const socketUrl = "ws://localhost:8080";
+
 function App() {
-  return <>Organizer</>;
+  const { sendMessage } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("opened"),
+    shouldReconnect: () => true,
+  });
+
+  const onSendClick = () => {
+    const randomMessage = (Math.random() + 1).toString(36).substring(7);
+    sendMessage(randomMessage);
+  };
+
+  return (
+    <>
+      <h1>Organizer</h1>
+      <br />
+      <button onClick={onSendClick}>Send random message</button>
+    </>
+  );
 }
 
 export default App;
